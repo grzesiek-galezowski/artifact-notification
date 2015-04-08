5bab7190cc666da39786d0edd9c8cf3c9347653a
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2015-04-08 23:19:46 +0200

    Added specification for path context

---
+++ DomainSpecification/PathContextSpecification.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+using NSubstitute;
+using NUnit.Framework;
+using Ports;
+using TddEbook.TddToolkit;
+
+namespace DomainSpecification
+{
+  public class PathContextSpecification
+  {
+    [Test]
+    public void ShouldUpdateMonitoredPathAndNotifyApplicationStartedWhenInitialized()
+    {
+      //GIVEN
+      var messages = Substitute.For<DiagnosticMessages>();
+      var watchers = Substitute.For<FileSystemWatchers>();
+      var presenter = Substitute.For<ApplicationEventsPresenter>();
+      var states = Any.Instance<PathStates>();
+      string description = Any.String();
+      
+      var context = new PathContext(messages, watchers, presenter, states);
+
+      watchers.Description().Returns(description);
+      
+      //WHEN
+      context.Initialize();
+
+      //THEN
+      Received.InOrder(() =>
+      {
+        presenter.UpdateMonitoredPath(watchers.Description());
+        messages.NotifyApplicationStarted();
+      });
+    }
+  }
+}
